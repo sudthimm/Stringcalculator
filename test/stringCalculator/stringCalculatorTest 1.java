1a8d8df93e7530f371a7786bcb590910ebe030de
sudthimm <sudhashreet@gmail.com>
2017-05-26 11:44:03 +0530

    new test added

---
+++ test/stringCalculator/stringCalculatorTest 1.java
@@ -0,0 +1,32 @@
+package stringCalculator;
+
+import static org.junit.Assert.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class stringCalculatorTest {
+
+	
+
+	@Test
+	public void addString() {
+	
+		
+	String s="1,2"; 
+		
+	System.out.println(s.charAt(0));	
+		assertEquals( 1 , CalculateSumOfString( s));
+		
+		
+		
+			}
+
+	
+	
+}
