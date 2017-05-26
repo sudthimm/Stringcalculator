a3b652e8668137f021de20471314f3bdadf17a85
Sudha <sudhashreet@gmail.com>
2017-05-26 09:53:19 +0530

    first commit

---
+++ src/stringCalculator/CalculateString.java
@@ -0,0 +1,25 @@
+package stringCalculator;
+
+
+
+
+public class CalculateString {
+	
+	int sum;
+	
+	
+private int CalculateSumOfString(String s) {
+		
+		return sum;
+	}
+
+	
+	
+	
+	
+	
+
+	
+	
+
+}
